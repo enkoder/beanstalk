cf0a3e2dfd6389b19f83890c48cadc4a733abe94
enkoder <kodiegoodwin@gmail.com>
2023-11-12 15:26:20 -0600

    WIP - results css

--- app/src/routes/results.tsx
+++ app/src/routes/results.tsx
@@ -5,12 +5,78 @@ import {
   UserService,
 } from "../client";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import "./Results.css";
 
 type ResultsParams = {
   user: string;
 };
 
+export function ResultRow(result: Result) {
+  return (
+    <>
+      <div style={{ height: "100px" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            overflow: "hidden",
+          }}
+        >
+          <Link to={`tournaments/${result.tournament_id}`}>
+            {result.tournament_name}
+          </Link>
+          <span>
+            <small style={{ paddingLeft: "25px" }}>
+              {result.registration_count} Players
+            </small>
+            <small style={{ paddingLeft: "10px" }}>
+              <a
+                href={
+                  "https://alwaysberunning.net/tournaments/" +
+                  result.tournament_id
+                }
+              >
+                ABR
+              </a>
+            </small>
+          </span>
+        </div>
+      </div>
+      <div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {result.corp_deck_url ? (
+            <small>
+              <a href={result.corp_deck_url}>
+                {/* TODO: replace the corp ID with the picture of the faction and the name of the ID */}
+                {result.corp_deck_identity_name}
+              </a>
+            </small>
+          ) : (
+            <small>{result.corp_deck_identity_name}</small>
+          )}
+          {result.runner_deck_url ? (
+            <small>
+              <a href={result.runner_deck_url}>
+                {/* TODO: replace the runner ID with the picture of the faction and the name of the ID */}
+                {result.runner_deck_identity_name}
+              </a>
+            </small>
+          ) : (
+            <small>{result.runner_deck_identity_name}</small>
+          )}
+        </div>
+      </div>
+      <div>
+        {result.rank_swiss}
+        {result.rank_cut ? " -> " + result.rank_cut : ""}
+      </div>
+      <div>
+        <span>{result.points_earned.toFixed(2)}</span>
+      </div>
+    </>
+  );
+}
 export function Results() {
   const params = useParams<ResultsParams>();
 
@@ -54,60 +120,14 @@ export function Results() {
               <></>
             )}
           </hgroup>
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th scope="col">Tournament</th>
-                  <th scope="col">Num Players</th>
-                  <th scope="col">Swiss</th>
-                  <th scope="col">Cut</th>
-                  <th scope="col">Runner</th>
-                  <th scope="col">Corp</th>
-                  <th scope="col">Points</th>
-                </tr>
-              </thead>
-              <tbody>
-                {results.results.map((result) => (
-                  <tr key={results.user_id + "/" + result.tournament_id}>
-                    <td>
-                      <a
-                        href={
-                          "https://alwaysberunning.net/tournaments/" +
-                          result.tournament_id
-                        }
-                      >
-                        {result.tournament_name}
-                      </a>{" "}
-                    </td>
-                    <td>{result.registration_count}</td>
-                    <td>{result.rank_swiss}</td>
-                    <td>{result.rank_cut}</td>
-                    <td>
-                      {result.runner_deck_url ? (
-                        <a href={result.runner_deck_url}>
-                          {/* TODO: replace the runner ID with the picture of the faction and the name of the ID */}
-                          {result.runner_deck_identity_name}
-                        </a>
-                      ) : (
-                        <text>{result.runner_deck_identity_name}</text>
-                      )}
-                    </td>
-                    <td>
-                      {result.corp_deck_url ? (
-                        <a href={result.corp_deck_url}>
-                          {/* TODO: replace the corp ID with the picture of the faction and the name of the ID */}
-                          {result.corp_deck_identity_name}
-                        </a>
-                      ) : (
-                        <text>{result.corp_deck_identity_name}</text>
-                      )}
-                    </td>
-                    <td>{result.points_earned.toFixed(2)}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <div className={"results-container"}>
+            <div className={"results-header-item"}>Tournament</div>
+            <div className={"results-header-item"}>Decks</div>
+            <div className={"results-header-item"}>Placement</div>
+            <div className={"results-header-item"}>Beans</div>
+            {results.results.map((result) => (
+              <ResultRow {...result} />
+            ))}
           </div>
         </>
       )}
