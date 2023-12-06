93be3f48a3a61e3f67b27f649f74e344747f2068
enkoder <kodiegoodwin@gmail.com>
2023-12-05 19:22:29 -0800

    use md

--- app/src/stories/Navbar.tsx
+++ app/src/stories/Navbar.tsx
@@ -81,9 +81,9 @@ export function Navbar() {
     <Disclosure as="nav" className="fixed z-[9999] w-full bg-gray-900">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-2 md:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -95,7 +95,7 @@ export function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <Link
                     to={"/"}
@@ -111,7 +111,7 @@ export function Navbar() {
                     <strong className={"pl-4 text-xl"}>Beanstalk</strong>
                   </Link>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden md:ml-6 md:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <Link
@@ -130,7 +130,7 @@ export function Navbar() {
                   </div>
                 </div>
               </div>
-              <div className="absolute  inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute  inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
                 {/* Bell icon, use this later once there is a blog
                 <button
                   type="button"
@@ -196,7 +196,7 @@ export function Navbar() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="md:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
                 <Disclosure.Button
