ed7be2b55621f050e5f6e1f5fbcf93a52bc00793
enkoder <kodiegoodwin@gmail.com>
2024-01-15 11:05:41 -0800

    Use nodemon to monitor openapi spec

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,26 +2,20 @@ module.exports = {
   apps: [
     {
       name: "app",
-      watch: false,
       script: "pnpm --filter app exec rspack serve",
     },
     {
       name: "api",
-      watch: false,
       script: "pnpm --filter api exec wrangler dev",
     },
     {
       name: "tailwind",
-      watch: false,
       script:
         "pnpm --filter app exec tailwindcss -i ./src/input.css -o ./src/output.css --watch",
     },
     {
       name: "spec",
-      watch: ["api/src/openapi.ts"],
-      script: "pnpm run spec",
-      autorestart: false,
-      stop_exit_codes: [0],
+      script: "pnpm exec nodemon --exec 'pnpm spec' --watch api/src/openapi.ts",
     },
   ],
 };
